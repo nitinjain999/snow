64d7d9e0206997a218efbce217574d30aa141a23
nitinjain999 <nitin_jain999@yahoo.co.in>
2021-02-12 16:22:53 +0100

    Adding Chart Dependancy

--- infra/terraform/charts.tf
+++ infra/terraform/charts.tf
@@ -20,3 +20,30 @@ resource "helm_release" "mysql" {
     value = "test_db_password"
   }
 }
+resource "helm_release" "myapp" {
+  name  = "myapp"
+  depends_on = [helm_release.mysql]
+  namespace = "snow"
+  chart = "../myapp_chart/myapp"
+  set {
+    name = "service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name = "secret.name"
+    value = "test_db"
+  }
+  set {
+    name = "secret.user"
+    value = "test_db_username"
+  }
+  set {
+    name = "secret.password"
+    value = "test_db_password"
+  }
+  set {
+    name = "secret.endpoint"
+    value = "mysql.snow"
+  }
+}
+
